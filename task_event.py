a143217768bf41839ee2939d4c3916de0d384539
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2021-09-11 01:33:45 -0400

    Implement TaskEvent

---
+++ task_event.py
@@ -0,0 +1,7 @@
+class TaskEvent:
+    def __init__(self, summary, description, date, start_time, end_time):
+        self.summary = summary
+        self.description = description
+        self.date = date
+        self.start_time = start_time
+        self.end_time = end_time
