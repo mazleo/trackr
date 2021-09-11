8bb8ac0544e9c9ec0a1b54f65bd20f56ad5e2ef8
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2021-09-11 11:34:51 -0400

    Add __str__ method to TaskEvent

--- task_event.py
+++ task_event.py
@@ -1,7 +1,9 @@
 class TaskEvent:
-    def __init__(self, summary, description, date, start_time, end_time):
+    def __init__(self, summary, description, start_time, end_time):
         self.summary = summary
         self.description = description
-        self.date = date
         self.start_time = start_time
         self.end_time = end_time
+    
+    def __str__(self):
+        return f"'summary': '{self.summary}',\n'description': '{self.description}',\n'start': {{\n\t'dateTime': '{self.start_time}'\n}},\n'end': {{\n\t'dateTime': '{self.end_time}'\n}}"
