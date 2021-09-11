9ea7c6c576a1c8a29eb556a55bc19a92b7416e87
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2021-09-11 11:35:20 -0400

    Complete TimesheetParser

--- timesheet_parser.py
+++ timesheet_parser.py
@@ -1,4 +1,5 @@
-import task_event
+from task_event import TaskEvent
+from event_datetime import EventDateTime
 
 import sys
 import csv
@@ -6,16 +7,17 @@ import datetime
 import time
 
 class TimesheetParser:
-
     @staticmethod
     def parse_csv_file_to_task_events(csv_filename):
         with open(csv_filename, newline='') as csv_file:
             timesheet_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
 
-            TimesheetParser.extract_csv_contents(timesheet_reader)
+            return TimesheetParser.extract_csv_contents(timesheet_reader)
     
     @staticmethod
     def extract_csv_contents(timesheet_reader):
+        task_events = []
+
         row_count = 0
         for row in timesheet_reader:
             row_count += 1
@@ -28,3 +30,27 @@ class TimesheetParser:
             project = row[3]
             task = row[4]
             comment = row[6]
+
+            task_event_summary = TimesheetParser.format_summary(project, task)
+            task_event_description = comment.strip()
+
+            start_end_ical = TimesheetParser.format_event_datetime_to_ical(date, start_time, end_time)
+            task_event_start_time = start_end_ical[0]
+            task_event_end_time = start_end_ical[1]
+
+            task_events.append(TaskEvent(task_event_summary, task_event_description, task_event_start_time, task_event_end_time))
+
+        return task_events
+
+    @staticmethod
+    def format_summary(project, task):
+        return project + ' | ' + task.replace('General | ', '')
+
+    @staticmethod
+    def format_event_datetime_to_ical(date, start_time, end_time):
+        event_datetime = EventDateTime(date, start_time, end_time)
+
+        start_datetime = datetime.datetime(event_datetime.year, event_datetime.month, event_datetime.day, hour=event_datetime.start_hour, minute=event_datetime.start_minute, second=event_datetime.start_second)
+        end_datetime = datetime.datetime(event_datetime.year, event_datetime.month, event_datetime.day, hour=event_datetime.end_hour, minute=event_datetime.end_minute, second=event_datetime.end_second)
+
+        return [start_datetime.isoformat(), end_datetime.isoformat()]
